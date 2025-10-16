27219c91f8a076a58f43e10fbe196a25171a7f1f
root <root@alpha.localdomain>
2025-10-16 20:49:47 -0300

    iniciate create product useCase

--- catalogservice/src/main/java/com/project/catalogservice/infrastruct/input/ProductController.java
+++ catalogservice/src/main/java/com/project/catalogservice/infrastruct/input/ProductController.java
@@ -5,22 +5,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.project.catalogservice.application.ports.input.CreateProduct;
+import com.project.catalogservice.application.ports.useCases.CreateProductUseCase;
+import com.project.catalogservice.domain.Product;
+import com.project.catalogservice.infrastruct.input.response.ProductRequest;
+import com.project.catalogservice.infrastruct.input.response.ProductResponse;
+
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    private final CreateProductUseCase createProductUseCase;
+    private final CreateProduct createProduct;
 
     @Autowired
-    public ProductController(CreateProductUseCase createProductUseCase) {
-        this.createProductUseCase = createProductUseCase;
+    public ProductController(CreateProduct createProduct) {
+        this.createProduct = createProduct;
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest product) {
-
-        Product createdProduct = createProductUseCase.execute(product);
-        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest request) {
+        Product createdProduct = createProduct.execute(Product.create(request.id(), request.name(), request.price(), request.description(), request.category()));
+        return new ResponseEntity<>(ProductResponse.fromDomain(createdProduct), HttpStatus.CREATED);
     }
 }
