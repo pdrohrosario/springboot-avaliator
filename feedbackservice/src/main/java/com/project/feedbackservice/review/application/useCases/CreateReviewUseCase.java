991cdccb6095d613dfb5b3590d7d8ba09ff967b8
Pedro Rosario <pdro.h.rosario@gmail.com>
2026-01-02 15:39:36 -0300

    fix error on import createReview interface

--- feedbackservice/src/main/java/com/project/feedbackservice/review/application/useCases/CreateReviewUseCase.java
+++ feedbackservice/src/main/java/com/project/feedbackservice/review/application/useCases/CreateReviewUseCase.java
@@ -3,7 +3,9 @@ package com.project.feedbackservice.review.application.useCases;
 import com.project.feedbackservice.review.application.input.CreateReviewInput;
 import com.project.feedbackservice.review.application.output.CreateReviewOutput;
 import com.project.feedbackservice.review.application.ports.input.CreateReview;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateReviewUseCase implements CreateReview {
 
     @Override
