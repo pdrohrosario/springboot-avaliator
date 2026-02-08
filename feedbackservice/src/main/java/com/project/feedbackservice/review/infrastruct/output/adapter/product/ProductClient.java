d9349c0f0be2cb5e07ec58928df0508d82b853c5
pedro.rosario <pdro.h.rosario@gmail.com>
2026-02-08 19:52:59 -0300

    fix feign client configuration error

--- feedbackservice/src/main/java/com/project/feedbackservice/review/infrastruct/output/adapter/product/ProductClient.java
+++ feedbackservice/src/main/java/com/project/feedbackservice/review/infrastruct/output/adapter/product/ProductClient.java
@@ -7,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalogservice", url = "http://catalogservice:8081", path = "/product", configuration = FeignAutoConfiguration.class)
+@FeignClient(value = "catalogservice", url = "http://catalogservice:8081")
 public interface ProductClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/product/{id}")
     public ResponseEntity<GetProductResponse> getProductById(@PathVariable("id") String id);
 }
