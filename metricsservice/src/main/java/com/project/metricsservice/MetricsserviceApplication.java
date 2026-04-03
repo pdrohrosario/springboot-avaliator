de3602bb819cccc75b13cbc2cfc8147d5d72b74e
Pedro Rosário <pdro.h.rosario@gmail.com>
2026-04-03 20:30:00 -0300

    add projects'starter

---
+++ metricsservice/src/main/java/com/project/metricsservice/MetricsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.project.metricsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsserviceApplication.class, args);
+	}
+
+}
---
+++ metricsservice/src/test/java/com/project/metricsservice/MetricsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.metricsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
